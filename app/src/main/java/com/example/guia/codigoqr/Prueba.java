25186ad34e8c793a6079000ef8b79e89e9a909bc
Karen <cindyocampo@hotmail.es>
2018-11-25 17:57:03 -0600

    Initial commit

---
+++ app/src/main/java/com/example/guia/codigoqr/Prueba.java
@@ -0,0 +1,13 @@
+package com.example.guia.codigoqr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Prueba extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prueba);
+    }
+}
